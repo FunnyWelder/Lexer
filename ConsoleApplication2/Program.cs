ed9c71d010c70c47634232b7a96f389c8253dc36
FunnyWelder <slavakuzm@mail.ru>
2021-12-18 10:59:04 +1000

    add table

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using static ConsoleApplication2.LexerTypes;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lexer lexer = new Lexer();
+            List<Token> tokens = lexer.Parse("input.swift");
+            Console.WriteLine($"tokens.size() {tokens.Count}");
+            foreach (Token token in tokens)
+            {
+                Console.WriteLine($"[{token.status}][{token.line}:{token.position}] {token.value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
